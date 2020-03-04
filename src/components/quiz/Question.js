d6b92a8fb0e0215cc4c0a12e176561b40339269b
Gustaf Ohlström <gustaf_ohlstrom@hotmail.com>
2020-03-04 17:57:33 +0100

    QG2-32 points displayed for each question

--- src/components/quiz/Question.js
+++ src/components/quiz/Question.js
@@ -2,11 +2,17 @@ import React from 'react'
 import Answers from './Answers'
 
 function Question(props) {
-    const { answers, question } = props.question
+    const { answers, question, correctAnswers, pointsPerAnswer } = props.question
     const { questionKey, onChange } = props
+    const maxPoints = correctAnswers.length * pointsPerAnswer
     return (
         <div className="mb-5">
-            <label className="question form-control mb-0">{question}</label>
+            <div class="question input-group">
+                <label className="form-control mb-0">{question}</label>
+                <div class="input-group-append">
+                    <span class="input-group-text">{maxPoints}</span>
+                </div>
+            </div>
             <div className="answers">
                 {answers.map((answer,index) => 
                     <Answers 
