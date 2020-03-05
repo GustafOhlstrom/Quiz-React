bb3a00a2a1f5f35b23cc4baca4d83e60d57d4d67
John Doeirma <irmaaa96@hotmail.com>
2020-03-05 15:02:21 +0100

    QG2-4 home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
+import {db} from "../config/fbConfig";
 
-class Home extends Component {
+class Home extends React.Component {
   state = {
-    quizes: null
+    quizes: ""
   };
 
+  componentDidMount() {
+    db.collection("quizes")
+      .get()
+      .then(snapshot => {
+        const quizes = [];
+        snapshot.forEach(doc => {
+          quizes.push({
+            id: doc.id,
+            ...doc.data()
+          });
+        });
+        this.setState({
+          quizes
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    console.log(this.state.quizes);
+    const title = this.state.quizes
+      ? this.state.quizes.map((q, i) => {
+          return <li key={i}>{q.title}</li>;
+        })
+      : "hello";
     return (
       <div>
-        <h1 className="text-center mt-4">Welcome to the Quiz Master</h1>
+        <ul>{title}</ul>
       </div>
     );
   }
 }
 
 export default Home;
+
